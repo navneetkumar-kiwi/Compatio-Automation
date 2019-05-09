22894e12196307a49cedeadfefe1b7c83e1d6c7c
navneetkumar-kiwi <35060141+navneetkumar-kiwi@users.noreply.github.com>
2019-05-09 22:13:17 +0530

    Login

--- src/main/java/pageObjectClasses/login_pom.java
+++ src/main/java/pageObjectClasses/login_pom.java
@@ -14,7 +14,7 @@ public class login_pom
 		PageFactory.initElements(driver, this);
 	}
 	
-/***ELEMENTS***/
+    /***ELEMENTS***/
 	
 	@FindBy(xpath= "//img[@src='/assets/images/compatio.png']")
 	public WebElement clogo;
@@ -24,4 +24,26 @@ public class login_pom
 	
 	@FindBy(xpath = "//label[contains(text(),'Email Address')]")
 	public WebElement email;
+	
+	@FindBy(xpath = "//input[@placeholder='name@company.com']")
+	public WebElement emailplc;
+	
+	@FindBy(xpath = "//label[contains(text(),'Password')]")
+	public WebElement pswrd;
+	
+	@FindBy(xpath = "//input[@placeholder='Enter a password']")
+	public WebElement pswrdplc;
+	
+	@FindBy(xpath = "//button[contains(text(),'Log In')]")
+	public WebElement loginbtn;
+	
+	@FindBy(xpath = "//a[contains(text(),'Forgot Password')]")
+	public WebElement forgot;
+	
+	@FindBy(xpath = "//div[contains(text(),'Don’t have an account?')]")
+	public WebElement account;
+	
+	@FindBy(xpath = "//a[contains(text(),'Request Access')]")
+	public WebElement rqstaccess;
+	
 }
--- src/test/java/testClasses/login_test.java
+++ src/test/java/testClasses/login_test.java
@@ -25,14 +25,39 @@ public class login_test
 		driver.manage().window().fullscreen();Thread.sleep(2000);
 	}
 	
-	@Test(enabled=true)
+	@Test(enabled=false)
 	public void a_elementsTest() throws InterruptedException
 	{
 		Assert.assertEquals("Compatio", driver.getTitle());
 		Assert.assertEquals(cp.clogo.isDisplayed() && cp.clogo.getSize().width==161 && cp.clogo.getSize().height==50, true);
 		Assert.assertEquals(cp.login.getText(), "Log In");
 		Assert.assertEquals(cp.email.getText(), "Email Address");
-		
+		Assert.assertEquals(cp.emailplc.isDisplayed(), true);
+		Assert.assertEquals(cp.pswrd.getText(), "Password");
+		Assert.assertEquals(cp.pswrdplc.isDisplayed(), true);
+		Assert.assertEquals(cp.loginbtn.getText(), "Log In");
+		Assert.assertEquals(cp.forgot.getText(), "Forgot Password?");
+		Assert.assertEquals(cp.account.getText(), "Don’t have an account? Request Access");
+	}
+	
+	@Test(enabled=false)
+	public void b_login() throws InterruptedException
+	{
+		cp.emailplc.sendKeys("navneet.kumar@kiwitech.com");Thread.sleep(3000);
+		cp.pswrdplc.sendKeys("Jaimaa26$");Thread.sleep(3000);
+		cp.loginbtn.click();Thread.sleep(3000);
+	}
+	
+	@Test(enabled=true)
+	public void c_forgot() throws InterruptedException
+	{
+		cp.forgot.click();Thread.sleep(3000);
+	}
+	
+	@Test(enabled=true)
+	public void d_rqst() throws InterruptedException
+	{
+		cp.rqstaccess.click();Thread.sleep(3000);
 	}
 	
 	@AfterMethod
